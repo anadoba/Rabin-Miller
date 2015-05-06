fcfe535fdb37a6b81466cb7dca8c0286c5f7ec20
Adam Nadoba <anadoba@windowslive.com>
2015-05-06 09:34:00 +0200

    wip

--- src/RabinMiller.java
+++ src/RabinMiller.java
@@ -1,4 +1,7 @@
+import javafx.util.converter.BigIntegerStringConverter;
+
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,20 +14,46 @@ public class RabinMiller {
 
     public static void main(String[] args) {
 
+        // TEST FERMATA
         if (args.length == 1 && args[0] == "-f") {
-            // test Fermata
+            String wejscie = czytajPlik(WEJSCIE);
+            List<BigInteger> lista = konwertujStringNaLiczby(wejscie);
+            String wynik = testFermata(lista.get(0));
+            zapiszDoPliku(WYJSCIE, wynik);
 
             return;
         }
 
+        // RABIN-MILLER
         if (args.length == 0) {
-            // właściwa część
+            String wejscie = czytajPlik(WEJSCIE);
+            List<BigInteger> lista = konwertujStringNaLiczby(wejscie);
 
             return;
         }
 
-
+        // ERROR
         System.out.println("Niewłaściwa komenda, dostępne wywołania:\n java RabinMiller -f\n java RabinMiller");
+    }
+
+    private static String testFermata(BigInteger bigInteger) {
+        // TODO
+
+        return "";
+    }
+
+    private static String rabinMiller(List<BigInteger> lista) {
+        // TODO
+
+        return "";
+    }
+
+    private static BigInteger najwiekszyWspolnyDzielnik(BigInteger a, BigInteger b) {
+        if (b == BigInteger.ZERO) {
+            return a;
+        } else {
+            return najwiekszyWspolnyDzielnik(b, a.mod(b));
+        }
     }
 
     private static String czytajPlik(String nazwaPliku) {
@@ -33,6 +62,17 @@ public class RabinMiller {
         } catch (IOException e) {
             throw new RuntimeException("Nie znaleziono pliku wejściowego!");
         }
+    }
+
+    private static List<BigInteger> konwertujStringNaLiczby(String string) {
+        List<BigInteger> listaBigInt = new ArrayList<>();
+        BigIntegerStringConverter bigIntegerStringConverter = new BigIntegerStringConverter();
+
+        for (String liczbaString : string.split("\n")) {
+            listaBigInt.add(bigIntegerStringConverter.fromString(liczbaString));
+        }
+
+        return listaBigInt;
     }
 
     private static void zapiszDoPliku(String nazwaPliku, String tekst) {
